24e17f2a875d5736c6b8a1f2536c091f216289ce
Jau <jaubass@gmail.com>
2022-12-28 18:08:52 +0100

    Ejercicio Funciones

---
+++ src/Ejercicio02/Funciones.java
@@ -0,0 +1,18 @@
+package Ejercicio02;
+
+public class Funciones {
+
+        public static void main(String[] args) {
+
+            double total = 0;
+            total = precioConIva(100.00d);
+            System.out.println("El precio con IVA es: " +total);
+
+
+        }
+
+        public static double precioConIva(double precio) {
+            double total = precio + (precio * 21 / 100);
+            return total;
+        }
+}
