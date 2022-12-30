62b09537ea7c4a0f71b3bb59419c8d88658c1b24
Jau <jaubass@gmail.com>
2022-12-30 22:34:30 +0100

    Ejercicios Tema 3

---
+++ src/Ejercicio03/EstructurasDeControl.java
@@ -0,0 +1,16 @@
+package Ejercicio03;
+
+public class EstructurasDeControl {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Jau", "Alba", "Ada", "Kai"};
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println("El nombre es: " +nombres[i]);
+
+        }
+
+    }
+
+}
